3d79519b7fb4961037abd7d7a80d8338c44ed820
n00b-sx <64547172+n00b-sx@users.noreply.github.com>
2023-02-03 21:49:02 +0800

    Update array.js

--- array.js
+++ array.js
@@ -6,3 +6,14 @@ guru[2] = "Opi";
 guru[3] = "Mey";
 
 console.log(guru);
+
+// 2. Menghapus Array
+guru[2] = undefined;
+
+console.log(guru);
+
+// 3. Menampilkan Array
+
+for (var i = 0; i < guru.length; i++) {
+  console.log(guru[i]);
+}
